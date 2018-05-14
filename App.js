bbfdd3a61c610edcdb30a7c664a9b1c7fa256857
Frank Dawes <jdawes22@gmail.com>
2018-05-14 07:24:06 +0100

    .gitignore update

--- App.js
+++ App.js
@@ -15,16 +15,9 @@ import MapPage from "./components/Map";
 import * as firebase from "firebase";
 import { StackNavigator } from "react-navigation";
 import NavigationService from "./NavigationService";
+import firebaseConfig from './config'
 
 // initialise firebase`
-const firebaseConfig = {
-  apiKey: "AIzaSyC1KleXVQ8TZGfs5BGrs1jeBV-4qS6BHQQ",
-  authDomain: "vinder-ed114.firebaseapp.com",
-  databaseURL: "https://vinder-ed114.firebaseio.com",
-  projectId: "vinder-ed114",
-  storageBucket: "vinder-ed114.appspot.com",
-  messagingSenderId: "562988374527"
-};
 
 firebase.initializeApp(firebaseConfig);
 
